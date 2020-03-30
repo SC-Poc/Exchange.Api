254b296c1164a4d17b3e2f24c26ea60b0e938f20
Alexey Novichikhin <novichikhin.au@gmail.com>
2020-03-30 19:03:02 +0300

    Split ocelot.json to several files Add ReRoute to operation service

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,54 +1,65 @@
 ﻿using System;
-using System.Configuration;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
-using System.Text;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Ocelot.DependencyInjection;
 using Swisschain.Sdk.Server.Common;
-using Swisschain.Sdk.Server.Configuration.WebJsonSettings;
-using Swisschain.Sdk.Server.Loggin;
+using Swisschain.Sdk.Server.Logging;
 
 namespace Api
 {
     public class Program
     {
+        private sealed class RemoteSettingsConfig
+        {
+            public IReadOnlyCollection<string> RemoteSettingsUrls { get; set; }
+        }
+
         public static void Main(string[] args)
         {
-            Console.Title = "Api Api";
+            Console.Title = "Exchange Api";
 
-            using (var loggerFactory = LogConfigurator.Configure("Api", ApplicationEnvironment.Config["SeqUrl"]))
+            var remoteSettingsConfig = ApplicationEnvironment.Config.Get<RemoteSettingsConfig>();
+
+            using var loggerFactory = LogConfigurator.Configure(
+                "Exchange",
+                ApplicationEnvironment.Config["SeqUrl"],
+                remoteSettingsConfig.RemoteSettingsUrls ?? Array.Empty<string>());
+
+            var logger = loggerFactory.CreateLogger<Program>();
+
+            try
             {
-                var logger = loggerFactory.CreateLogger<Program>();
+                logger.LogInformation("Application is being started");
 
-                try
-                {
-                    logger.LogInformation("Application is being started");
+                CreateHostBuilder(loggerFactory, remoteSettingsConfig).Build().Run();
 
-                    CreateHostBuilder(loggerFactory).Build().Run();
-
-                    logger.LogInformation("Application has been stopped");
-                }
-                catch (Exception ex)
-                {
-                    logger.LogCritical(ex, "Application has been terminated unexpectedly");
-                }
+                logger.LogInformation("Application has been stopped");
+            }
+            catch (Exception ex)
+            {
+                logger.LogCritical(ex, "Application has been terminated unexpectedly");
             }
         }
 
-        private static IHostBuilder CreateHostBuilder(ILoggerFactory loggerFactory) =>
+        private static IHostBuilder CreateHostBuilder(ILoggerFactory loggerFactory, RemoteSettingsConfig remoteSettingsConfig) =>
             new HostBuilder()
                 .SwisschainService<Startup>(options =>
                 {
                     options.UseLoggerFactory(loggerFactory);
-                    options.WithWebJsonConfigurationSource(ApplicationEnvironment.Config["RemoteSettingsUrl"]);
+
+                    var urls = remoteSettingsConfig?.RemoteSettingsUrls ?? new[] { ApplicationEnvironment.Config["RemoteSettingsUrl"] };
+                    options.AddWebJsonConfigurationSources(urls);
                     
                 }, w =>
                 {
 
-                }, c =>
+                }, (context, c) =>
                 {
                     var remoteOcelotHost = ApplicationEnvironment.Config["RemoteOcelotConfigHost"];
                     if (!string.IsNullOrEmpty(remoteOcelotHost))
@@ -66,6 +77,51 @@ namespace Api
 
                         //c.AddWebJsonConfiguration(url);
                     }
-                });
+
+                    //c.AddOcelot(c);
+                    // c.AddOcelot((IWebHostEnvironment)context.HostingEnvironment);
+                    
+                })
+                .ConfigureWebHost(builder =>
+                    {
+                        builder.ConfigureAppConfiguration((context, configurationBuilder) =>
+                        {
+                            LoadOcelotFile("ocelot", context);
+                            LoadOcelotFile("ocelot.temp", context);
+
+                            configurationBuilder.AddOcelot(context.HostingEnvironment);
+                        });
+                    });
+
+        private static void LoadOcelotFile(string name,
+            WebHostBuilderContext context)
+        {
+            try
+            {
+                var remoteOcelotHost = ApplicationEnvironment.Config["RemoteOcelotConfigHost"];
+                if (!string.IsNullOrEmpty(remoteOcelotHost))
+                {
+                    if (remoteOcelotHost.Last() != '/')
+                        remoteOcelotHost += "/";
+
+                    using var client = new HttpClient();
+
+                    var url = $"{remoteOcelotHost}{name}";
+                    var json = client.GetStringAsync(url).Result;
+
+                    var rootPath = context.HostingEnvironment.ContentRootPath;
+                    var fileName = $"{name}.json";
+
+                    using var writer = new StreamWriter(Path.Combine(rootPath, fileName), false);
+                    writer.WriteLine(json);
+                    writer.Flush();
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"Error on load ocelot ");
+            }
+
+        }
     }
 }
